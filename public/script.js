ea4758a921dd60e1990b8cf55e9b12c7ab2158ff
DEINXH <gyx020905@gmail.com>
2023-06-04 04:12:15 +1000

    change public to docs

--- public/script.js
+++
@@ -1,9 +0,0 @@
-function addItem(){
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "block";
-  }
-  function hidePopup(){
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "none";
-  }
-  
