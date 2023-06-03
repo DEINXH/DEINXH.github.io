24856ae89a573cf9834930d2f7900ae89f4ecbf3
DEINXH <gyx020905@gmail.com>
2023-06-04 04:30:36 +1000

    doc to public

---
+++ public/script.js
@@ -0,0 +1,9 @@
+function addItem(){
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "block";
+  }
+  function hidePopup(){
+    var overlay = document.getElementById("overlay");
+    overlay.style.display = "none";
+  }
+  
