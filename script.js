90a035a87b5d651e4c33e2aad0d757752771e564
DEINXH <gyx020905@gmail.com>
2023-06-04 21:48:28 +1000

    icon changing

--- script.js
+++ script.js
@@ -21,3 +21,37 @@ function dateID(){
   document.getElementById("Date").innerHTML = date;
 }
   
+function iconChange(){
+var icons = document.getElementById('icon');
+var type= document.getElementById('skinType').value;
+icon.className=type
+}
+
+
+const form=document.getElementById('inputform')
+
+inputform.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  
+  var name= document.getElementById('skinName').value;
+  var type= document.getElementById('skinType').value;
+  var BOS= document.querySelector('input[name="BOS"]:checked').value;
+  var skinValue= document.getElementById('Value').value;
+  var note= document.getElementById('note').value;
+  var id = document.getElementById('ID').value;
+  var date = document.getElementById('Date').value;
+  
+  
+
+  var logo="";
+  if(type==='gun'){
+    logo='<img src="./icon/gun.png" style="width:50px;height:auto;>'
+  }else if(type==='knife'){
+    logo='<img src="./icon/knife.png" style="width:50px;height:auto;>'
+  }else if(type==='glove'){
+    logo='<img src="./icon/glove.png" style="width:50px;height:auto;>'
+  }
+
+  
+  var itemCard ;
+})
