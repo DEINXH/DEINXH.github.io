fe8240c4dbf99a669f75b9da14ec7358df150530
DEINXH <gyx020905@gmail.com>
2023-06-04 20:01:10 +1000

    ID and Date

--- README.md
+++
@@ -1,3 +0,0 @@
-# Developer Document
-
-This is a demo application running on a basic node server using the express framework.
--- script.js
+++ script.js
@@ -2,8 +2,22 @@ function addItem(){
     var overlay = document.getElementById("overlay");
     overlay.style.display = "block";
   }
-  function hidePopup(){
+  
+function hidePopup(){
     var overlay = document.getElementById("overlay");
     overlay.style.display = "none";
   }
+
+
+function randomID() {
+  var Rand = Math.random()
+  var mineId = Math.round(Rand * 100000000)
+ document.getElementById("ID").innerHTML = "ID:"+mineId;
+}
+
+function dateID(){
+  let time= new Date().toISOString();
+  var date=/\d{4}-\d{1,2}-\d{1,2}/g.exec(time); /* Remove hours, minutes and seconds */
+  document.getElementById("Date").innerHTML = date;
+}
   
